cba00d281c79d9c80d3e02fc43230170cf0ca03c
Clément Colin <clement@MacBook-Pro-de-Clement.local>
2024-10-20 12:49:00 +0900

    WIP: parsing

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ccolin <ccolin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 11:32:16 by ccolin            #+#    #+#             */
-/*   Updated: 2024/10/19 17:41:58 by ccolin           ###   ########.fr       */
+/*   Updated: 2024/10/20 12:48:33 by ccolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,104 @@ char	*clear_input(char *input)
 		while (new_input[j - 1] == ' ' || new_input[j] == '\t')
 			j--;
 	new_input[j] = '\0';
-	ft_printf("\ninput = |%s|", input);
-	ft_printf("\ninput = |%s|\n", new_input);
+	ft_printf("\ninput =\n\t\t|%s|", input);
+	ft_printf("\ncleared input =\n\t\t|%s|\n", new_input);
 	return (new_input);
+}
+
+void	print_tokens(char **tokens)
+{
+	int	i;
+
+	i = 0;
+	while (tokens && tokens[i])
+	{
+		printf("Token %d: %s\n", i, tokens[i]);
+		i++;
+	}
+}
+
+void	skip_over_quotes(char *input, int *i)
+{
+	if (input[*i] == '\'')
+	{
+		(*i)++;
+		while (input[*i] != '\'')
+		(*i)++;
+	}
+	if (input[*i] == '"')
+	{
+		(*i)++;
+		while (input[*i] != '"')
+		(*i)++;
+	}
+}
+
+int	array_size(char **array)
+{
+	int	i;
+
+	i = 0;
+	if (!array)
+		return (0);
+	while (array[i])
+		i++;
+	return (i);
+}
+char	**add_str_to_arr(char **array, char *new_string)
+{
+	int		i;
+	int		size;
+	char	**new_array;
+	
+	i = 0;
+	size = array_size(array);
+	new_array = malloc(sizeof(char *) * (size + 2));
+	if (!new_array)
+		return (NULL);
+	while (i < size)
+	{
+		new_array[i] = array[i];
+		i++;
+	}
+	new_array[size] = new_string;
+	new_array[size + 1] = NULL;
+	free (array);
+	return (new_array);
+}
+
+char	**add_token(char **tokens, char *input, int start, int *i)
+{
+	if (input[*i] == ' ')
+		tokens = add_str_to_arr(tokens, ft_substr(input, start, *i - start));
+	return (tokens);
 }
 
 char	**tokenize(char *input, int i)
 {
+	char	**tokens;
+	int		start;
+
+	ft_printf("Starting tokenization of string \"%s\"\n", input);
+	tokens = NULL;
+	start = i;
+	while (input[i])
+	{
+		skip_over_quotes(input, &i);
+		if (ft_strchr(" |<>", input[i]))
+		{
+			ft_printf("string to add = %s, start = %d, end = %d\n", input, start, i);
+			tokens = add_token(tokens, input, start, &i);
+			start = i + 1;
+		}
+		i++;
+	}
+	if (start < i)
+	{
+		ft_printf("Adding last token, string to add = %s, start = %d, end = %d\n", input, start, i);
+		tokens = add_str_to_arr(tokens, ft_substr(input, start, i - start));
+	}
+	print_tokens(tokens);
 	return (NULL);
 }
 
