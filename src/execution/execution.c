6f5cc1b22012b29d7fd5cf73ebc10528ff7cbb90
Kakizaki55 <minoka.kakizaki@gmail.com>
2024-10-27 14:15:21 +0900

    testing again

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccolin <ccolin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mkakizak <mkakizak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 10:04:40 by ccolin            #+#    #+#             */
-/*   Updated: 2024/10/25 12:40:33 by ccolin           ###   ########.fr       */
+/*   Updated: 2024/10/27 14:15:13 by mkakizak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 int	main_execution(char **envp)
 {
 	t_cmnd_tbl	table;
+
+
+
+
 
 	ft_printf("Starting execution mode\n-------------\n\n");
 	init_cmnd_tbl(&table);
