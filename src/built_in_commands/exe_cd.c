4b3d6c7cf315ca9ccb4634507ca9b0869d714d28
Kakizaki55 <minoka.kakizaki@gmail.com>
2024-11-25 21:07:15 +0900

    WIP started working on cd and error status, it seems like the catching the error status for the pipes ane working, but need to implement the error handing manualy for the builtin commands

--- src/built_in_commands/exe_cd.c
+++ src/built_in_commands/exe_cd.c
@@ -6,7 +6,7 @@
 /*   By: mkakizak <mkakizak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 19:09:16 by mkakizak          #+#    #+#             */
-/*   Updated: 2024/11/22 16:20:03 by mkakizak         ###   ########.fr       */
+/*   Updated: 2024/11/25 21:04:12 by mkakizak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,49 @@
 
 void exe_cd(t_command *cmd, t_cmnd_tbl *table)
 {
-	// this need error handeling for more then 1 args
-	// need go to home if '~'
-	if(cmd->args == NULL || cmd->args[0] == NULL)
+	// this needs error handling for more than 1 args
+	// need to go to home if '~'
+
+	char *home_dir;
+	char *new_dir;
+
+	home_dir = getenv("HOME");
+	new_dir = NULL;
+
+	if (cmd->args == NULL || cmd->args[0] == NULL)
 	{
-		chdir(getenv("HOME"));
-		return ;
+		chdir(home_dir);
+		return;
 	}
 	else
 	{
-		chdir(cmd->args[0]);
+		char *path = cmd->args[0];
+
+		if (path[0] == '~')
+		{
+			if (path[1] == '\0' || path[1] == '/')
+			{
+				if(path[2] == '\0')
+				{
+					puts("home with out path");
+					chdir(home_dir);
+				}
+			}
+			else
+			{
+				puts("home with path");
+				// puts("does it make it here?");
+				// new_dir = ft_calloc(ft_strlen(home_dir) + ft_strlen(path), sizeof(char));
+				// // ft_strlcat(new_dir, home_dir, ft_strlen(home_dir));
+				// // ft_strlcat(new_dir, &path[2], ft_strlen(home_dir));
+				// ft_printf("new_dir: %s\n", new_dir);
+				// chdir(new_dir);
+				// free(new_dir);
+				return;
+			}
+		}
+		ft_printf("just path: %s\n", path);
+		chdir(path);
 	}
 	puts("YOU MADE IT TO CD!!");
 }
