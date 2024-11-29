826f2a3458f905efbcebf9cb6bf879df83dc7916
Kakizaki55 <minoka.kakizaki@gmail.com>
2024-11-29 17:32:00 +0900

    clean up

--- src/development_tools/print_command_tables.c
+++ src/development_tools/print_command_tables.c
@@ -6,7 +6,7 @@
 /*   By: mkakizak <mkakizak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 13:27:06 by ccolin            #+#    #+#             */
-/*   Updated: 2024/11/29 13:23:23 by mkakizak         ###   ########.fr       */
+/*   Updated: 2024/11/29 17:30:32 by mkakizak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ void	print_command_arrays(char **array, char *arg_type)
 {
 	int	i;
 
-	printf("Command %s:\n", arg_type); fflush(stdout);
+	printf("Command %s:\n", arg_type);
 	if (array)
 	{
 		i = 0;
 		while (array[i])
 		{
-			printf("\t%s[%d]: %s\n", arg_type, i, array[i]); fflush(stdout);
+			printf("\t%s[%d]: %s\n", arg_type, i, array[i]);
 			i++;
 		}
 	}
 	else
-		printf("\tNo %s.\n", arg_type); fflush(stdout);
+		printf("\tNo %s.\n", arg_type);
 }
 
 void	print_command(t_command *cmd)
@@ -36,21 +36,21 @@ void	print_command(t_command *cmd)
 
 	if (!cmd)
 		return ;
-	printf("Command: %s\n", cmd->command ? cmd->command : "None"); fflush(stdout);
+	printf("Command: %s\n", cmd->command ? cmd->command : "None");
 	print_command_arrays(cmd->args, "arg");
 	print_command_details(cmd);
 }
 
 void	print_command_details(t_command *cmd)
 {
-	printf("Input file: %s\n", cmd->input_file ? cmd->input_file : "None");fflush(stdout);
+	printf("Input file: %s\n", cmd->input_file ? cmd->input_file : "None")
 	printf("Heredoc delimiter: %s\n",
-		cmd->heredoc_delimiter ? cmd->heredoc_delimiter : "None"); fflush(stdout);
+		cmd->heredoc_delimiter ? cmd->heredoc_delimiter : "None");
 	print_command_arrays(cmd->output_file, "output");
 	print_command_arrays(cmd->append, "append");
-	printf("Pipe In: %d\n", cmd->pipe_in);fflush(stdout);	
-	printf("Pipe Out: %d\n", cmd->pipe_out);fflush(stdout);
-	printf("Is Built-in: %d\n", cmd->is_built_in);fflush(stdout);
+	printf("Pipe In: %d\n", cmd->pipe_in)	
+	printf("Pipe Out: %d\n", cmd->pipe_out)
+	printf("Is Built-in: %d\n", cmd->is_built_in)
 	printf("----------\n\n");
 }
 
@@ -62,8 +62,8 @@ void	print_cmnd_tbl(t_cmnd_tbl *table)
 		return ;
 	if (!table)
 		return ;
-	printf("Exit Status: %d\n", table->last_exit_status); fflush(stdout);
-	printf("Exit shell: %d\n", table->exit_shell);fflush(stdout);
+	printf("Exit Status: %d\n", table->last_exit_status);
+	printf("Exit shell: %d\n", table->exit_shell)
 	// print_environment_variables(table);
 	print_commands(table);
 }
@@ -72,7 +72,7 @@ void	print_commands(t_cmnd_tbl *table)
 {
 	t_command	*current;
 
-	printf("\nCommands:\n"); fflush(stdout);
+	printf("\nCommands:\n");
 	current = table->head;
 	while (current)
 	{
