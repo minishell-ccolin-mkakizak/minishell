c287d2647a0d1712c74de1cec04adfb363a52a5d
Kakizaki55 <minoka.kakizaki@gmail.com>
2024-12-02 15:36:53 +0900

    another bug fix

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: mkakizak <mkakizak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 10:00:00 by mkakizak          #+#    #+#             */
-/*   Updated: 2024/12/02 14:40:57 by mkakizak         ###   ########.fr       */
+/*   Updated: 2024/12/02 15:35:45 by mkakizak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,21 @@ void signal_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
-		// write(STDOUT_FILENO, "\n", 1);
-		printf("SIGINT is: [%d]\n\n", sig);
+		write(STDOUT_FILENO, "\n", 1);
+		// printf("SIGINT is: [%d]\n\n", sig);
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
 	else if (sig == SIGQUIT)
 	{
-		printf("SIGQUIT is: [%d]\n\n", sig);
+		// printf("SIGQUIT is: [%d]\n\n", sig);
 		rl_on_new_line();
 		rl_redisplay();
 	}
 	else if (sig == SIGTERM)
 	{
-		printf("SIGTERM is: [%d]\n\n", sig);
+		// printf("SIGTERM is: [%d]\n\n", sig);
 		// write(STDOUT_FILENO, "\nReceived SIGTERM\n", 17);
 		exit(EXIT_SUCCESS);
 	}
