288f4ddd65068c9162f68aabdd1ec8a3fbf84f53
ccolin <clmnt.co@gmail.com>
2024-12-13 12:51:16 +0900

    fixed everything hopefully

--- src/built_in_commands/exe_exit.c
+++ src/built_in_commands/exe_exit.c
@@ -6,7 +6,7 @@
 /*   By: ccolin <ccolin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 19:15:13 by mkakizak          #+#    #+#             */
-/*   Updated: 2024/12/13 12:42:55 by ccolin           ###   ########.fr       */
+/*   Updated: 2024/12/13 12:46:55 by ccolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,20 +38,17 @@ void	exit_with_error_numeric(t_command *cmd)
 void	exe_exit(t_command *cmd, t_cmnd_tbl *table)
 {
 	unsigned int	exit_status;
-	unsigned int	exit_status;
 
 	exit_status = 0;
 	printf("exit\n");
-	exit_status = 0;
-	exit_status = ft_atoi(cmd->args[0]);
-	printf("exit_status: %d\n", exit_status);
-	if (cmd->args[0] == NULL)
-	{
+	if (!cmd || !cmd->args)
 		exit(EXIT_SUCCESS);
-	}
+	if (cmd->args[0] == NULL)
+		exit(EXIT_SUCCESS);
 	if (!is_all_digits(cmd->args[0]))
 		exit_with_error_numeric(cmd);
-	exit_status = (unsigned int)atoi(cmd->args[0]);
+	exit_status = (unsigned int)ft_atoi(cmd->args[0]);
+	printf("exit_status: %d\n", exit_status);
 	if (cmd->args[1] != NULL)
 	{
 		printf("minishell: exit: too many arguments\n");
