41ecb25821c264dd13f135420b02ce3253e787ab
ccolin <clmnt.co@gmail.com>
2024-12-13 12:19:23 +0900

    fixed exit segfault

--- src/built_in_commands/exe_exit.c
+++ src/built_in_commands/exe_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exe_exit.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkakizak <mkakizak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: ccolin <ccolin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 19:15:13 by mkakizak          #+#    #+#             */
-/*   Updated: 2024/12/12 17:33:03 by mkakizak         ###   ########.fr       */
+/*   Updated: 2024/12/13 12:18:59 by ccolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,31 +26,29 @@ int is_all_digits(const char *str)
 	return (TRUE);
 }
 
-void	exe_exit(t_command *cmd, t_cmnd_tbl *table)
+void exe_exit(t_command *cmd, t_cmnd_tbl *table)
 {
-	(void)cmd;
-	(void)table;
+    unsigned int exit_status = 0;
+
 	printf("exit\n");
-
-	unsigned int exit_status = 0;
-	exit_status = atoi(cmd->args[0]);
-	printf("exit_status: %d\n", exit_status);
-
-	if (cmd->args[0] == NULL)
+    if (!cmd || !cmd->args) {
+        exit(EXIT_SUCCESS);
+    }
+    if (cmd->args[0] == NULL) {
+        exit(EXIT_SUCCESS);
+    }
+    if (!is_all_digits(cmd->args[0])) 
 	{
-		exit(EXIT_SUCCESS);
-	}
-	else if(cmd->args[1] != NULL)
+        printf("minishell: exit: %s: numeric argument required\n", cmd->args[0]);
+        exit(2);
+    }
+    exit_status = (unsigned int)atoi(cmd->args[0]);
+    if (cmd->args[1] != NULL) 
 	{
-		printf("minishell: exit: too many arguments\n");
-		table->last_exit_status = EXIT_FAILURE;
-		return ;
-	}
-	else if(exit_status == 0 || !is_all_digits(cmd->args[0]))
-	{
-		printf("minishell: exit: ");
-		printf("%s: numeric argument required\n", cmd->args[0]);
-		exit(2);
-	}
-	exit(exit_status);
+        printf("minishell: exit: too many arguments\n");
+        if (table)
+            table->last_exit_status = EXIT_FAILURE;
+        return;
+    }
+    exit(exit_status);
 }
